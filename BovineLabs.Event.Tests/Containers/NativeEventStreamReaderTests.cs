39415c1fedcb6bff6beae9cab73251cb4ce1d00c
Timothy Raines <tim@ninjatertle.com>
2020-11-05 08:14:20 +1100

    Split thread and index tests.

---
+++ BovineLabs.Event.Tests/Containers/NativeEventStreamReaderTests.cs
@@ -0,0 +1,67 @@
+// <copyright file="NativeEventStreamReaderTests.cs" company="BovineLabs">
+//     Copyright (c) BovineLabs. All rights reserved.
+// </copyright>
+
+#if BL_TESTING
+
+namespace BovineLabs.Event.Tests.Containers
+{
+    using System;
+    using BovineLabs.Event.Containers;
+    using NUnit.Framework;
+    using Unity.Collections;
+    using Unity.Entities.Tests;
+    using Unity.Jobs.LowLevel.Unsafe;
+
+    internal partial class NativeEventStreamTests
+    {
+        internal class Reader : ECSTestsFixture
+        {
+#if ENABLE_UNITY_COLLECTIONS_CHECKS
+            /// <summary> Ensures that reading with begin throws an exception. </summary>
+            [Test]
+            public void ReadWithoutBeginThrows()
+            {
+                var stream = new NativeEventStream(Allocator.Temp);
+                stream.AsThreadWriter().Write(0);
+
+                var reader = stream.AsReader();
+                Assert.Throws<ArgumentException>(() => reader.Read<int>());
+
+                stream.Dispose();
+            }
+
+            /// <summary> Ensures that begin reading out of range throws an exception. </summary>
+            [Test]
+            public void BeginOutOfRangeThrows()
+            {
+                var stream = new NativeEventStream(Allocator.Temp);
+
+                var reader = stream.AsReader();
+                Assert.Throws<ArgumentOutOfRangeException>(() => reader.BeginForEachIndex(-1));
+                Assert.Throws<ArgumentOutOfRangeException>(() =>
+                    reader.BeginForEachIndex(JobsUtility.MaxJobThreadCount + 1));
+
+                stream.Dispose();
+            }
+
+            /// <summary> Ensures reading past the end throws an exception. </summary>
+            [Test]
+            public void TooManyReadsThrows()
+            {
+                var stream = new NativeEventStream(Allocator.Temp);
+                stream.AsThreadWriter().Write(0);
+
+                var reader = stream.AsReader();
+                reader.BeginForEachIndex(0);
+                reader.Read<int>();
+                Assert.Throws<ArgumentException>(() => reader.Read<int>());
+
+                stream.Dispose();
+            }
+#endif
+        }
+    }
+}
+
+#endif
