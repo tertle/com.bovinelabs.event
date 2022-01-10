a3a12182d68bff7dde9b49f033ae72c84c322c01
Timothy Raines <tim@ninjatertle.com>
2022-01-10 21:48:36 +1100

    Added Schedule and ScheduleParallel methods that take readers instead of drawinstances to allow sending same job from a job.

--- BovineLabs.Event/Jobs/IJobEventReader.cs
+++ BovineLabs.Event/Jobs/IJobEventReader.cs
@@ -67,6 +67,34 @@ namespace BovineLabs.Event.Jobs
 
         /// <summary> Schedule a <see cref="IJobEventReader"/> job. </summary>
         /// <param name="jobData"> The job. </param>
+        /// <param name="readers"> The readers. </param>
+        /// <param name="dependsOn"> The job handle dependency. </param>
+        /// <typeparam name="TJob"> The type of the job. </typeparam>
+        /// <typeparam name="T"> The type of the key in the hash map. </typeparam>
+        /// <returns> The handle to job. </returns>
+        public static unsafe JobHandle Schedule<TJob>(this TJob jobData, UnsafeReadArray<NativeEventStream.Reader> readers, JobHandle dependsOn = default)
+            where TJob : struct, IJobEventReader
+        {
+            for (var i = 0; i < readers.Length; i++)
+            {
+                var fullData = new EventJobReaderStruct<TJob> { Reader = readers[i], JobData = jobData, Index = i };
+
+                var scheduleParams = new JobsUtility.JobScheduleParameters(
+                    UnsafeUtility.AddressOf(ref fullData),
+                    EventJobReaderStruct<TJob>.Initialize(),
+                    dependsOn,
+                    ScheduleMode.Parallel);
+
+                dependsOn = JobsUtility.Schedule(ref scheduleParams);
+            }
+
+            readers.Dispose(dependsOn);
+
+            return dependsOn;
+        }
+
+        /// <summary> Schedule a <see cref="IJobEventReader"/> job. </summary>
+        /// <param name="jobData"> The job. </param>
         /// <param name="consumer"> The consumer. </param>
         /// <param name="dependsOn"> The job handle dependency. </param>
         /// <typeparam name="TJob"> The type of the job. </typeparam>
@@ -102,6 +130,38 @@ namespace BovineLabs.Event.Jobs
 
             readers.Dispose(dependsOn);
             consumer.AddJobHandle(dependsOn);
+
+            return dependsOn;
+        }
+
+        /// <summary> Schedule a <see cref="IJobEventReader"/> job. </summary>
+        /// <param name="jobData"> The job. </param>
+        /// <param name="readers"> The readers. </param>
+        /// <param name="dependsOn"> The job handle dependency. </param>
+        /// <typeparam name="TJob"> The type of the job. </typeparam>
+        /// <typeparam name="T"> The type of the key in the hash map. </typeparam>
+        /// <returns> The handle to job. </returns>
+        public static unsafe JobHandle ScheduleParallel<TJob>(
+            this TJob jobData, UnsafeReadArray<NativeEventStream.Reader> readers, JobHandle dependsOn = default)
+            where TJob : struct, IJobEventReader
+        {
+            var input = dependsOn;
+
+            for (var i = 0; i < readers.Length; i++)
+            {
+                var fullData = new EventJobReaderStruct<TJob> { Reader = readers[i], JobData = jobData, Index = i };
+
+                var scheduleParams = new JobsUtility.JobScheduleParameters(
+                    UnsafeUtility.AddressOf(ref fullData),
+                    EventJobReaderStruct<TJob>.Initialize(),
+                    input,
+                    ScheduleMode.Parallel);
+
+                var handle = JobsUtility.Schedule(ref scheduleParams);
+                dependsOn = JobHandle.CombineDependencies(dependsOn, handle);
+            }
+
+            readers.Dispose(dependsOn);
 
             return dependsOn;
         }
